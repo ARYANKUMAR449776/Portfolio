15202b0111b026c56db17eaec6afd1a9e789eaed
Aryan Kumar <aimpendance@gmail.com>
2025-05-08 14:09:04 -0600

    Version 1 Deploy No Works

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
+import { use, useState, useEffect } from 'react'
 import './App.css'
+import NavBar from '../src/Components/Navbar.jsx'
 
 function App() {
+  const [message, setMessage] = useState('')
 
+  useEffect(() => {
+    fetch('/api/message')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
   return (
+
     <>
-      
+      <NavBar />
+      <h1>My React App, {message}</h1>
     </>
   )
 }
