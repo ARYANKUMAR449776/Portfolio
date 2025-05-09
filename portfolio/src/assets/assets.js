0f7e73fa3d6cc862976f02ec00444bc07a459370
Aryan Kumar <aimpendance@gmail.com>
2025-05-09 02:27:55 -0600

    V3

--- portfolio/src/assets/assets.js
+++ portfolio/src/assets/assets.js
@@ -1,4 +1,4 @@
-import LogoWhite from '../assets/logo-white.png';
+import LogoWhite from '../assets/Logo-White.png';
 import MainBodyImage1 from '../assets/MainBodyImage1.png';
 import MainBodyImage2 from '../assets/MainBodyImage2.png';
 export const assets = {
