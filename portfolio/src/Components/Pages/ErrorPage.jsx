97d5dbe392faa1181de3ef0dc95e549aeec6cdcb
Aryan Kumar <aimpendance@gmail.com>
2025-05-13 16:22:20 -0600

    Acceptable push changes

--- portfolio/src/Components/Pages/ErrorPage.jsx
+++ portfolio/src/Components/Pages/ErrorPage.jsx
@@ -5,10 +5,9 @@ import './ErrorPage.css';
 const ErrorPage = () => {
   return (
     <div className="error-page-wrapper">
-      <div className="error-page">
-        <h1 className='TopHeading'>404 - Page Under Construction</h1>
-        <img src={assets.ErrorPageLogo} className="ErrorImage" alt="Error Page" />
-      <h1 className='BottomHeading'>Contact aimpendance@gmail.com for more</h1>
+      <div className="error-page-heading">
+        <h1 className='TopHeading'>404 - Page Under Construction :)</h1>
+        <h1 className='BottomHeading'>Contact aimpendance@gmail.com for more</h1>
       </div>
     </div>
   );
